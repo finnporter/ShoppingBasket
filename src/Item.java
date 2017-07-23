156e0c7f7979c3203b6d898e60f140fccc6f8808
finnporter <iamfinnporter@gmail.com>
2017-07-23 12:01:50 +0100

    sets up Item getters

--- src/Item.java
+++ src/Item.java
@@ -2,4 +2,26 @@
  * Created by Finn on 23/07/2017.
  */
 public class Item {
+
+    private String name;
+    private double price;
+    private String description;
+
+    public Item(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
